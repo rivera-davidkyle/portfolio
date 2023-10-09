4a5e2fcc80faa0805762ae81904997756aa6b88c
rivera-davidkyle <rivera.davidkyle@gmail.com>
2023-10-08 20:28:11 -0400

    Updates

---
+++ precache-manifest.ecda933336a0623527e27cfde78f0739.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f345f432fb32c4ad770",
+    "url": "/my-website/static/css/main.d216b35d.chunk.css"
+  },
+  {
+    "revision": "4f345f432fb32c4ad770",
+    "url": "/my-website/static/js/main.088cde58.chunk.js"
+  },
+  {
+    "revision": "fe2c2329e96ea778d0ef",
+    "url": "/my-website/static/js/runtime~main.c00519ff.js"
+  },
+  {
+    "revision": "70a55e03ae51e4fabc93",
+    "url": "/my-website/static/js/2.b18a7a08.chunk.js"
+  },
+  {
+    "revision": "c97d5298308c8959e269",
+    "url": "/my-website/static/js/3.f3412412.chunk.js"
+  },
+  {
+    "revision": "1de980854269621fe6e18e6bb01f5d99",
+    "url": "/my-website/static/media/pfp.1de98085.png"
+  },
+  {
+    "revision": "8a31043366f71ec262acd5355822c34b",
+    "url": "/my-website/static/media/resume.8a310433.pdf"
+  },
+  {
+    "revision": "8ec1feaea2c27d4875053398447b970a",
+    "url": "/my-website/index.html"
+  }
+];
